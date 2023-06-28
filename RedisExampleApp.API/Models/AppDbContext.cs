877df37313094a1708134fed291bcfe4ae2aa270
Mert <79603812+ufcovo@users.noreply.github.com>
2023-06-28 20:25:22 +0300

    DbContext and Product are created
    DbContext and Product are created

---
+++ RedisExampleApp.API/Models/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RedisExampleApp.API.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                new Product() { Id = 1, Name = "Pencil 1", Price = 10 },
+                new Product() { Id = 2, Name = "Pencil 2", Price = 11 },
+                new Product() { Id = 3, Name = "Pencil 3", Price = 12 }
+                );
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- RedisExampleApp.API/Program.cs
+++ RedisExampleApp.API/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using RedisExampleApp.API.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseInMemoryDatabase("myDatabase");
+});
 
 var app = builder.Build();
 
